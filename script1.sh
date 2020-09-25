4cdc676d274005be11a5e856732a7ed9434b0ab3
Laura Carroll <lcarroll916@gmail.com>
2020-09-25 10:36:07 -0400

    Hello world!

---
+++ script1.sh
@@ -0,0 +1,5 @@
+# This is the script for Tutorial 7 where we're practicing Bioinformatics
+# Usage: bash script1.sh <argument>
+# This needs to be run in the directory with x, y, z data files
+
+echo "Hello world!"
